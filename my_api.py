4d297b8e423d666975c0fb2de559ee9ff32f5da8
sacosent <cosentinosantiago92@gmail.com>
2024-05-30 16:27:35 -0300

    Se agregan 2 nuevos endpoints a la API para obtener usuarios por filtro

--- my_api.py
+++ my_api.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from typing import List
+from fastapi import FastAPI, HTTPException, Query
+from typing import List, Optional
 from user_db import User, user_list
 
 app = FastAPI()
@@ -7,9 +7,30 @@ app = FastAPI()
 #--------------------- USER LOGIN ---------------------#
 
 @app.get("/")
-def root():
+async def root():
     return {"Hello": "World"}
 
 @app.get("/users", response_model=List[User])
-def get_users():
+async def users():
     return user_list
+
+@app.get("/users/listfilter", response_model=List[User])
+def get_users_by_ids(user_ids: Optional[List[int]] = Query(None)):
+    if not user_ids:
+        return user_list
+    
+    filtered_users = [user for user in user_list if user.id in user_ids]
+    
+    if not filtered_users:
+        raise HTTPException(status_code=404, detail="No users found with the provided IDs")
+    
+    return filtered_users
+
+@app.get("/users/rangefilter", response_model=List[User])
+def get_users_by_range(start_id: int, end_id: int):
+    filtered_users = [user for user in user_list if start_id <= user.id <= end_id]
+    
+    if not filtered_users:
+        raise HTTPException(status_code=404, detail="No users found within the provided range")
+    
+    return filtered_users
