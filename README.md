ba04366c44eee450661a4412d4f745fdce88add4
sacosent <cosentinosantiago92@gmail.com>
2024-06-06 11:01:28 -0300

    Actualización de README y limpieza de archivos y código

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 - **ReDoc Documentation**: http://127.0.0.1:8000/redoc
 - **OpenAPI Schema**: http://127.0.0.1:8000/openapi.json
 
+# Descarga versión community: https://www.mongodb.com/try/download
+# Instalación:https://www.mongodb.com/docs/manual/tutorial
+# Módulo conexión MongoDB: pip install pymongo
+# Ejecución: sudo mongod --dbpath "/path/a/la/base/de/datos/"
+# Conexión: mongodb://localhost
+
+# Despliegue API en la nube:
+# Deta - https://www.deta.sh/
+# Intrucciones - https://fastapi.tiangolo.com/deployment/deta/
+
 .
 #### Make sure to first run the following command to start your local server:
 uvicorn main:app --reload
--- database/db_connection.py
+++ database/db_connection.py
@@ -1,17 +1,20 @@
 
-from pymongo import MongoClient
-
-# URI to connect to mongodb free server
-uri = "mongodb+srv://cosentinosantiago92:ZVIxJad2oGIFQ2sz@timetracker.60srdxz.mongodb.net/?retryWrites=true&w=majority&appName=TimeTracker"
+### MongoDB client ###
+from pymongo import MongoClient, server_api
 
 # Create a new client and connect to the server
 dbclient = MongoClient() # This connects to localhost
-# client = MongoClient(uri, server_api=ServerApi('1')) # This connects to Mongo DB Atlas Server
 
+#-------------- Base de datos remota MongoDB Atlas (https://mongodb.com) --------------#
+## URI to connect to mongodb free server
+# uri = "mongodb+srv://cosentinosantiago92:ZVIxJad2oGIFQ2sz@timetracker.60srdxz.mongodb.net/?retryWrites=true&w=majority&appName=TimeTracker"
+# dbclient = MongoClient(uri, server_api=server_api('1')) # This connects to Mongo DB Atlas Server
+## dbclient = MongoClient("mongodb+srv://<user>:<password>@<url>/?retryWrites=true&w=majority").test
 
 # Send a ping to confirm a successful connection
-try:
+""" try:
     dbclient.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+"""
